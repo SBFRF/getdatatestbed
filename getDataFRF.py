237240440f1faf42eacd170efd17f67cbec0d99a
Spicer Bak <spicer.bak.frf@gmail.com>
2021-11-18 17:27:52 -0500

    some changes, don't know

--- getDataFRF.py
+++ getDataFRF.py
@@ -1324,7 +1324,6 @@ class getObs:
         elif str(gaugenumber).lower() in ['lidarwavegauge140']:
             self.dataloc = "oceanography/waves/lidarWaveGauge140/lidarWaveGauge140.ncml"
 
-
         else:
             self.gname = 'There Are no Gauge numbers here'
             raise NameError('Bad Gauge name, specify proper gauge name/number, or add capability')
